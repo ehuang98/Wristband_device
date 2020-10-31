d806a21afc4931f3cb03cc2ee6cf72f4e46d41fe
Eric Huang <100619405@oncampus.local>
2020-10-31 00:01:36 -0400

    Initial commit

---
+++ app/src/main/java/com/capstone/bodyareanetwork/ui/status/StatusViewModel.java
@@ -0,0 +1,17 @@
+package com.capstone.bodyareanetwork.ui.status;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class StatusViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public StatusViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is status fragment");
+    }
+
+    public LiveData<String> getText() { return mText; }
+}
